ebb5aef9fa0287f0d7e9d735f5521e79a641b1dc
Matus Kysel <matus.kysel@sophiatx.com>
2018-08-22 15:04:02 +0200

    Updated version

--- balance_checker.py
+++ balance_checker.py
@@ -4,19 +4,23 @@ import requests
 import smtplib
 import sched, time
 import sys
+import datetime
 from websocket import create_connection
 
 ws = create_connection("ws://127.0.0.1:9195/")
 s = sched.scheduler(time.time, time.sleep)
+
+refresh_rate = 6 * 60 * 60 # 6h
 smtp_pwd = ""
 smtp_user = "notification@sophiatx.com"
 smtp_server = "localhost"
-email_array = ["matus.kysel@sophiatx.com"]
+email_array = ["matus.kysel@sophiatx.com", "marian.rajnic@sophiatx.com"]
 config = json.load(open("config.json", "r"))
 
 def get_balance(account):
     ws.send(json.dumps({"jsonrpc": "2.0", "method": "get_account_balance", "params":[str(account)], "id": 0 }))
-    return round(float(json.loads(ws.recv())["result"]) / 1000000, 6) 
+    out = json.loads(ws.recv())
+    return round(float(out["result"]) / 1000000, 6) 
 
 def send_email(subject, msg):
     server = smtplib.SMTP(smtp_server)
@@ -37,13 +41,17 @@ def send_email(subject, msg):
 def run_bot():
     msg = ""
     for account in config:
-        if(get_balance(account["name"]) < account["trash_hold"]):
-            msg = account["name"] + " balace is under " + str(account["thrash_hold"]) + 
-            " (" +str(get_balance(account["name"])) + ") \n"
+        if(get_balance(account["name"]) < account["thrash_hold"]):
+            msg = account["name"] + " balance is under " + str(account["thrash_hold"]) + " (" +str(get_balance(account["name"])) + ") \n"
 
     if(msg != ""):
-        send_email("One or more accounts have no enought tokens", msg)
+        print("#####################################################################################")
+        print(datetime.datetime.now())
+        print("#####################################################################################")
+        print(msg)
+        # send_email("One or more accounts have no enought tokens", msg)
 
+s.enter(refresh_rate, 1, run_bot)
 
 if __name__ == "__main__":
     s.enter(1, 1, run_bot)
